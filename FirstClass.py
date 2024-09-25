a5c87cfb00ae643a69447fc31fa1873bfdf607a4
Muhammad Ammar <146322576+Muhammar933@users.noreply.github.com>
2024-09-25 18:30:46 +0500

    Add files via upload

---
+++ FirstClass.py
@@ -0,0 +1,16 @@
+#Basic Calculator using Just Variables and Data Types
+## Take two numbers as input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the Second number: "))
+
+#Perform Basic operation using operators
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+#print the Result of operations 
+print("Addition Result: ", addition)
+print("Subtraction Result: ", subtraction)
+print("Multiplication Result: ", multiplication)
+print("Division Result: ", division)
